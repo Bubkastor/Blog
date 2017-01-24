2595ae5ab25301a2179de22178e5566f5f90edae
bubok <bubok@DESKTOP-EMC4A6P>
2017-01-24 16:29:48 +0300

    minimiz css

--- src/WebApplication2/gulpfile.js
+++ src/WebApplication2/gulpfile.js
@@ -3,8 +3,8 @@ var gulp = require('gulp');
 sass = require('gulp-sass');
 gulp.task('sass', function() {    
     gulp.src('./wwwroot/sass/**/*.scss')
-        //.pipe(sass({outputStyle: 'compressed'}))
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}))
+        //.pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./wwwroot/css'));
     });
 gulp.task('default', ['sass'], function() {
